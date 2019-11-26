6e07d874fff3ef97657eed822b2265b19a2a734a
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2019-11-26 21:08:20 +0530

    Gatsby Starter Pack

--- src/components/globals/Footer.js
+++ src/components/globals/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { styles } from '../../utils';
-import { FaInstragram, FaTwitter, FaFacebook } from 'react-icons/fa';
+import { FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa';
 export default class Footer extends Component {
 	state = {
 		icons: [
@@ -12,7 +12,7 @@ export default class Footer extends Component {
 			},
 			{
 				id: 2,
-				icon: <FaInstragram className='icon instagram-icon' />,
+				icon: <FaInstagram className='icon instagram-icon' />,
 				path: `http://instagram.com`
 			},
 			{
@@ -25,13 +25,51 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<FooterWrapper>
-				<div className='title'>Eatery</div>
+				<div className='title'>eCommerce</div>
 				<div className='icons'>
-					<a href='' />
+					{this.state.icons.map((item) => (
+						<a href={item.path} key={item.id} target='_blank' rel='noopener noreferrer'>
+							{item.icon}
+						</a>
+					))}
 				</div>
+				<p className='copyright'>copyright &copy; 2019 eCommerce</p>
 			</FooterWrapper>
 		);
 	}
 }
 
-const FooterWrapper = styled.footer``;
+const FooterWrapper = styled.footer`
+	padding: 2rem 0;
+	background: ${styles.colors.mainBlack};
+	.icons {
+		width: 10rem;
+		display: flex;
+		justify-content: space-between;
+		margin: 0 auto;
+	}
+	.icon {
+		color: ${styles.colors.mainWhite};
+		font-size: 1.3rem;
+		${styles.transition({})};
+		&:hover {
+			color: ${styles.colors.mainYellow};
+		}
+	}
+	.copyright {
+		color: ${styles.colors.mainWhite};
+		text-transform: capitalize;
+		text-align: center;
+		margin: 1rem 0;
+	}
+	.title {
+		text-align: center;
+		width: 10rem;
+		color: ${styles.colors.mainYellow};
+		text-transform: uppercase;
+		padding: 0.3rem 1rem;
+		margin: 0 auto 2rem auto;
+		font-size: 1.2rem;
+		${styles.border({ color: `${styles.colors.mainYellow}` })};
+	}
+`;
