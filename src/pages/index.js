b8e459a2073b4b8906fbffbb6c513ca4c16be32d
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2019-11-27 14:16:17 +0530

    Gallery Modified and Finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import QuickInfo from '../components/HomePageComponent/QuickInfo';
-import Gallery from '../components/HomePageComponent/Gallery';
+import Gallery from '../components/HomePageComponent/GalleryMulti';
 import { HomeHeader, Banner, BannerButton } from '../utils';
 import img from '../images/bcg/homeBcg.jpeg';
 
